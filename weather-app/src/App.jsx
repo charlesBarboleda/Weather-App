c293839e8ee9b40cad474e12c08c35b7099f21e1
charlesBarboleda <166056415+charlesBarboleda@users.noreply.github.com>
2024-04-22 13:03:46 -0700

    Finished main container
    main container init

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Axios from 'axios'
 import { useState } from 'react'
+import Weather from './components/Weather'
 
 function App() {
 
@@ -8,7 +9,7 @@ function App() {
   const [ location, setLocation] = useState("")
 
   const API_KEY = "ab9ec1d191fefda968263ca546fa087c"
-  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${API_KEY}`
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&limit=3&appid=${API_KEY}`
 
   const searchLocation = (event) => {
     if(event.key === "Enter") {
@@ -26,7 +27,7 @@ function App() {
         <div className="text-center p-4">
           <input 
           type="text" 
-          className="py-3 px-6 w-[20rem] text-lg rounded-3xl border border-gray-200 
+          className="text-center w-[15rem] h-[2rem] text-lg rounded-3xl border border-gray-200 
           text-gray-600 placeholder:text-gray-400 focus:outline-none bg-white-600/100 
           shadow-md"
           placeholder="Enter Location"
@@ -34,6 +35,7 @@ function App() {
           onChange={(event) => setLocation(event.target.value)}
           onKeyDownCapture={searchLocation} />
       </div>
+      <Weather weatherData = {data} />
       </div>
      
   )
--- weather-app/src/components/Weather.jsx
+++ weather-app/src/components/Weather.jsx
@@ -1,10 +1,56 @@
 import React from 'react'
 
-const Weather = ({weatherData}) => {
-    console.log(weatherData);
+const Weather = ( {weatherData} ) => {
   return (
-    <div>
-      Weather
+    <div >
+       {weatherData.weather ? (
+          <div className="w-[20rem] h-[10rem] bg-gray-600 shadow-lg rounded-xl m-auto relative px-6 top-[10%]">
+            <div className="flex justify-between w-full">
+              <div className="w-1/2 my-4 mx-auto flex justify-between items-center">
+                <div className="flex flex-col items-start justify-between h-full">
+                  <div>
+                    <p className="text-xl">
+                      {weatherData.name},
+                      {weatherData.sys.country}
+                    </p>
+                  </div>
+                  <div>
+                    <h1 className="text-4xl font-semibold pb-4">
+                        {weatherData.main.temp.toFixed()
+                        }°C
+                    </h1>
+                  </div>
+                </div>
+              </div>
+              <div className="w-1/2 flex flex-col justify-between items-end">
+                <div className="relative">
+                  <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="" className="w-[4rem]"/>
+                </div>
+              {weatherData.name !== undefined ? (
+                <div className="flex flex-col justify-evenly my-4 mx-auto text-xs">
+                  <div className="flex justify-between">
+                    <p>Feels Like</p>
+                    <p className="font-bold w-10 gap-x-8">{weatherData.main.feels_like.toFixed()}°C</p>
+                  </div>
+                  <div className="flex justify-between gap-x-8">
+                    <p>Humidity</p>
+                    <p className="font-bold w-10">{weatherData.main.humidity}%</p>
+                  </div>
+                  <div className="flex justify-between gap-x-8">
+                    <p>Wind Speed</p>
+                    <p className="font-bold w-10">{weatherData.wind.speed.toFixed()}KPH </p>
+                  </div>
+                  <div className="flex justify-between gap-x-8">
+                    <p>Pressure</p>
+                    <p className="font-bold w-10">{weatherData.main.pressure}hPa </p>
+                  </div>
+                </div>
+              ): null}
+              </div>
+            </div>
+          </div>
+
+       ): null}
     </div>
   )
 }
